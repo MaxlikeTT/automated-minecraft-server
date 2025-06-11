5774ba8087c69345e1ebfef276feb51d67f8cfaf
Max Taylor <max.taylor12@yahoo.com>
2025-06-11 15:45:22 -0700

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-west-2"  # change if you're using a different region
+}
+
+resource "aws_instance" "minecraft_server" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Ubuntu 20.04 for us-west-2 (x86)
+  instance_type = "t3.small"
+
+  vpc_security_group_ids = [aws_security_group.minecraft_sg.id]
+  key_name               = "minecraft-key"  # won't be used since no SSH
+
+  tags = {
+    Name = "minecraft-server"
+  }
+}
+
+resource "aws_security_group" "minecraft_sg" {
+  name        = "minecraft_sg"
+  description = "Allow Minecraft port"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # allow everyone
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
